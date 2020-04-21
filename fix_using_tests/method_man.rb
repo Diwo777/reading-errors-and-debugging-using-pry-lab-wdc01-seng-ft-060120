492c32d1556550a33ccf8cefd52606c0b8cd8880
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-21 00:28:45 +0000

    Automatically backed up by Learn

---
+++ fix_using_tests/method_man.rb
@@ -0,0 +1,9 @@
+def start_game(player1, player2)
+  greeting = "Hello #{player1} & #{player2}"
+  greeting
+end
+
+def play_game(player1, player2)
+ greets = "#{player1} is better than #{player2}"
+ greets
+end
